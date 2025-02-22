bbed9f0a043c74c3b40184898de30e4f7093296a
pavelcermak1 <35462448-pavelcermak1@users.noreply.replit.com>
2025-02-22 10:39:09 +0000

    User checkpoint: Update Next.js build; rebuild project.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bbc8f79d3f946dbe"; }
+/******/ 		__webpack_require__.h = function() { return "a0101b9482dadebe"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
