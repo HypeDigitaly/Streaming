dd605ac5ad5726ae6d3c1edd43468c69ae93ccc9
pavelcermak1 <35462448-pavelcermak1@users.noreply.replit.com>
2025-02-24 17:44:35 +0000

    User checkpoint: Update Next.js application build; likely minor styling or content changes.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7b25114ef9784738"; }
+/******/ 		__webpack_require__.h = function() { return "5f5ca9d350655045"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
