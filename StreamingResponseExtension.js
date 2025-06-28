4f5857417bc6521be4befaa855f20f3547a738eb
Pavel Čermák <pavelcermak@hypedigitaly.ai>
2025-06-28 17:25:47 +0000

    U

--- StreamingResponseExtension.js
+++ StreamingResponseExtension.js
@@ -131,14 +131,14 @@ export const StreamingResponseExtension = {
           .response-content h3,
           .response-content h4,
           .response-content h5 {
-            margin: 0.6em 0 0.3em 0;
+            margin: 0.2em 0 0.1em 0;
             font-weight: 600;
           }
-          .response-content h1 { font-size: 1.3em; margin: 0.8em 0 0.4em 0; font-weight: 600; }
-          .response-content h2 { font-size: 1.2em; margin: 0.7em 0 0.3em 0; font-weight: 600; }
-          .response-content h3 { font-size: 1.1em; margin: 0.6em 0 0.3em 0; font-weight: 600; }
-          .response-content h4 { font-size: 1.05em; margin: 0.5em 0 0.2em 0; font-weight: 600; }
-          .response-content h5 { font-size: 1em; margin: 0.4em 0 0.2em 0; font-weight: 600; }
+          .response-content h1 { font-size: 1.3em; margin: 0.3em 0 0.15em 0; font-weight: 600; }
+          .response-content h2 { font-size: 1.2em; margin: 0.25em 0 0.1em 0; font-weight: 600; }
+          .response-content h3 { font-size: 1.1em; margin: 0.2em 0 0.1em 0; font-weight: 600; }
+          .response-content h4 { font-size: 1.05em; margin: 0.15em 0 0.08em 0; font-weight: 600; }
+          .response-content h5 { font-size: 1em; margin: 0.1em 0 0.05em 0; font-weight: 600; }
           
           /* First headings in container should not have top margin */
           .response-content > h1:first-child,
@@ -154,7 +154,7 @@ export const StreamingResponseExtension = {
           .response-content h2 + h3,
           .response-content h3 + h4,
           .response-content h4 + h5 {
-            margin-top: 0.15em;
+            margin-top: 0.05em;
           }
           
           /* Remove any paragraphs between headers */
@@ -202,7 +202,7 @@ export const StreamingResponseExtension = {
             line-height: 1;
           }
           .response-content p {
-            margin: 0 0 0.3em 0;
+            margin: 0 0 0.1em 0;
           }
           .response-content p:last-child {
             margin-bottom: 0;
@@ -230,7 +230,7 @@ export const StreamingResponseExtension = {
             height: 1px;
             background-color: #E5E7EB;
             width: 100%;
-            margin: 0.4em 0;
+            margin: 0.15em 0;
           }
           /* Added styles for headings */
           .response-content .answer-h1 {
