34ecb2bf42c3a051e4f4a26364a134b8b29465d9
Pavel Čermák <pavelcermak@hypedigitaly.ai>
2025-07-14 09:52:32 +0000

    gemini flash 2.5flash without nitro

--- StreamingResponseExtension.js
+++ StreamingResponseExtension.js
@@ -2075,7 +2075,7 @@ export const StreamingResponseExtension = {
       },
       {
         id: 20,
-        name: "google/gemini-2.5-flash-lite-preview-06-17:nitro",
+        name: "google/gemini-2.5-flash",
         type: "openrouter",
         endpoint: "/api/openrouter-stream",
         displayName: "Gemini 2.5 Flash Lite 06-17 (OpenRouter)",
