c99a2cf66020b7153386bc8398b11d657c8455b4
HypeDigitaly s.r.o. <pavelcermak@hypedigitaly.ai>
2025-10-28 16:11:54 +0000

    added vysocinapecuje.cz as whitelisted

--- config/domains.js
+++ config/domains.js
@@ -1,25 +1,24 @@
-
 // Shared whitelist domains configuration
 const whitelistedDomains = [
-  'icuk.cz',
-  'kr-ustecky.cz',
-  'kr-vysocina.cz',
-  'setrivodou.cz',
-  'healthytwenty.cz',
-  'barber-mnb.cz',
-  'teplice.cz',
-  'hypedigitaly.ai',
-  'litomerice.cz',
-  'khk.cz',
-  'stredoceskykraj.cz',
-  'usti.cz',
-  'strekov.cz',
-  'nestemice.eu',
-  'severni-terasa.cz',
-  'mesto-ul.cz',
-  'kr-karlovarsky.cz'
+  "icuk.cz",
+  "kr-ustecky.cz",
+  "kr-vysocina.cz",
+  "vysocinapecuje.cz",
+  "healthytwenty.cz",
+  "barber-mnb.cz",
+  "teplice.cz",
+  "hypedigitaly.ai",
+  "litomerice.cz",
+  "khk.cz",
+  "stredoceskykraj.cz",
+  "usti.cz",
+  "strekov.cz",
+  "nestemice.eu",
+  "severni-terasa.cz",
+  "mesto-ul.cz",
+  "kr-karlovarsky.cz",
 ];
 
 module.exports = {
-  whitelistedDomains
+  whitelistedDomains,
 };
